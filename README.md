bea3e66f1367727e8933634363094c2cecd2e0bb
OmriGM <omrigr123@gmail.com>
2021-11-16 01:52:21 +0200

    - Started implementation and testing of winking with img input - If the img includes a right winking eye, the page will scroll down - Added math functions for calculating EAR (eye aspect ratio) and a method for calculating distance between 2 points.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # wink-browser-controller
+
+Resources:
+Inspiration for calculating winks/blinks: https://www.pyimagesearch.com/2017/04/24/eye-blink-detection-opencv-python-dlib/
--- src/App.js
+++ src/App.js
@@ -1,104 +1,130 @@
 import { useState, useEffect } from 'react'
-import logo from './logo.svg';
+import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+import '@tensorflow/tfjs-backend-webgl';
 import './App.css';
+
+const EYE_ASPECT_RATIO_TH = 0.35
+const EYE_AR_CONSEC_FRAMES = 3
 
 const App = () => {
     const [model, setModel] = useState(null);
 
+    const calculateEyeAspectRatio = (upperEye, lowerEye) => {
+        const a = calculateDistance(upperEye[4], lowerEye[3]);
+        const b = calculateDistance(upperEye[5], lowerEye[4]);
 
-    useEffect(() => {
+        const c = calculateDistance(upperEye[0], upperEye[upperEye.length - 1]);
+        const ear = (a + b) / (2.0 * c);
+        return ear
+    }
 
+    const calculateDistance = (point1, point2) => Math.hypot(point2[0] - point1[0], point2[1] - point1[1])
+
+    useEffect(async () => {
+        const model = await faceLandmarksDetection.load(
+            faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);
+        console.log('model loaded');
+        const predictions = await model.estimateFaces({
+            input: document.querySelector('img')
+        });
+        predictions.forEach(({ annotations }) => {
+            const { leftEyeUpper0, leftEyeLower0, rightEyeUpper0, rightEyeLower0 } = annotations
+            console.log(annotations);
+            const leftEyeAspectRatio = calculateEyeAspectRatio(leftEyeUpper0, leftEyeLower0)
+            const rightEyeAspectRatio = calculateEyeAspectRatio(rightEyeLower0, rightEyeUpper0)
+            if(rightEyeAspectRatio <= EYE_ASPECT_RATIO_TH) {
+                window.scrollTo({
+                    top: 200,
+                    behavior: 'smooth'
+                });
+                console.log('right eye wink!')
+            }
+            if(leftEyeAspectRatio <= EYE_ASPECT_RATIO_TH) console.log('left eye wink!')
+            console.log(leftEyeAspectRatio);
+            console.log(rightEyeAspectRatio);
+        })
+        // for each face
+        // calculate the eye aspect ratio for each eye
+        // if it's less than TH, it's a blink
+        // check if it's a wink (and determine which eye to make sure it's a wink and not a blink by using the EAR
+        // value which should be bigger than the TH
+        // Interact with the browser upon a wink by the user choice (Can be go back/forward or scrolling down/up in
+        // the same page, or get more ideas)
+        // Optional:
+        // let the user enable video frame presented at the top left
+        // create a chrome extension from it
+        // Add a counter for: blinks, right wink, left wink
+        // Add animations
+        // Present a winking emoji matching the wink side on the face
     }, [])
 
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras auctor sodales tempus. Sed ante
-                    libero, scelerisque a pulvinar non, mattis eu nibh. Donec bibendum euismod lorem quis laoreet.
-                    Aenean elit leo, pretium vel condimentum quis, porta vel lorem. Suspendisse cursus ante sed purus
-                    hendrerit, in porta mauris vehicula. Duis quis egestas ipsum, quis porta neque. Curabitur vulputate
-                    felis ac ex iaculis facilisis. Fusce euismod, velit ut interdum vehicula, massa ligula vehicula mi,
-                    in ultricies ex nulla sit amet tellus. Suspendisse porttitor tellus velit, id vestibulum dolor
-                    pharetra at. Nullam tempor mi aliquam neque rhoncus feugiat. Suspendisse id cursus velit, ac cursus
-                    urna. Donec ut placerat lectus. Fusce sit amet malesuada turpis, et posuere est. In ut congue
-                    tortor, id aliquet sapien.
+            <img crossOrigin='anonymous'
+                 src={'https://qph.fs.quoracdn.net/main-qimg-5cc7395de4daa5267f0de0ec1e9571dc-c'}
+                 alt="logo"/>
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+            <p>
 
-                    Donec nec fermentum sapien, sit amet ullamcorper nisi. Mauris sagittis quis quam sed porttitor.
-                    Quisque lobortis a ante posuere dignissim. Donec accumsan accumsan pharetra. Aliquam erat volutpat.
-                    Quisque pellentesque, orci at laoreet efficitur, urna elit lobortis tellus, in facilisis eros sapien
-                    a felis. Donec cursus efficitur leo. Integer non nisi id magna ullamcorper facilisis. Aenean sed leo
-                    sagittis, pharetra diam non, vulputate risus. Integer risus enim, semper in tincidunt sed, hendrerit
-                    et ligula. Aliquam consequat, dolor consequat laoreet bibendum, dolor nisi iaculis diam, eget auctor
-                    mi felis et ex. Nunc aliquam enim vitae lorem faucibus, in efficitur nibh malesuada. Pellentesque
-                    sed purus sed massa consequat volutpat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras auctor sodales tempus. Sed ante
+                libero, scelerisque a pulvinar non, mattis eu nibh. Donec bibendum euismod lorem quis laoreet.
+                Aenean elit leo, pretium vel condimentum quis, porta vel lorem. Suspendisse cursus ante sed
+                purus
+                hendrerit, in porta mauris vehicula. Duis quis egestas ipsum, quis porta neque. Curabitur
+                vulputate
+                felis ac ex iaculis facilisis. Fusce euismod, velit ut interdum vehicula, massa ligula vehicula
+                mi,
+                in ultricies ex nulla sit amet tellus. Suspendisse porttitor tellus velit, id vestibulum dolor
+                pharetra at. Nullam tempor mi aliquam neque rhoncus feugiat. Suspendisse id cursus velit, ac
+                cursus
+                urna. Donec ut placerat lectus. Fusce sit amet malesuada turpis, et posuere est. In ut congue
+                tortor, id aliquet sapien.
+            </p>
+            <p>
 
-                    Ut semper in metus quis imperdiet. Integer sapien tellus, aliquam a finibus vitae, semper et lacus.
-                    Donec vitae justo et orci dapibus scelerisque at id sem. In pharetra est eu ante volutpat feugiat.
-                    Fusce elementum augue et nisi vulputate lacinia. Donec vitae dignissim velit. Mauris massa odio,
-                    dignissim a mattis et, vehicula ut massa. Donec dolor eros, blandit eget efficitur ac, vestibulum et
-                    dolor. Maecenas eleifend mi augue, vel tempus leo commodo ut. Fusce vitae eleifend nulla. Ut
-                    molestie lacus velit, eu pellentesque justo finibus a. In eu lorem pharetra, aliquet sem ultrices,
-                    facilisis ligula.
+                Suspendisse dolor ante, finibus at euismod quis, porttitor et nulla. In erat eros, tempus a
+                facilisis sed, faucibus consequat est. Mauris venenatis enim ut ante molestie, feugiat convallis
+                libero eleifend. Praesent sodales, ligula tempus fringilla iaculis, elit ex posuere lorem, ut
+                lacinia turpis ex et orci. Proin varius mauris quam, sit amet rutrum ante dictum sed. Sed non
+                metus
+                sed dolor suscipit tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac
+                gravida sem. Suspendisse potenti.
+            </p>
+            <p>
 
-                    Sed ornare, ligula eu blandit accumsan, enim nulla molestie diam, vel dapibus nisl ex eget mauris.
-                    Aliquam efficitur, felis ut tincidunt sollicitudin, erat lorem fringilla ex, vel ultrices sem massa
-                    ac leo. Donec in ultrices ipsum. Mauris in ultricies dui, non dictum massa. Orci varius natoque
-                    penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas lobortis ullamcorper
-                    elementum. Fusce sollicitudin leo et blandit viverra. Integer et dui laoreet, iaculis mauris a,
-                    fermentum odio. Aenean interdum felis sit amet augue volutpat finibus. Proin volutpat sollicitudin
-                    vulputate. Sed placerat eleifend ipsum non accumsan. Morbi volutpat varius augue, a efficitur nulla
-                    pulvinar non. Nam tellus magna, ullamcorper nec turpis in, faucibus congue est.
+                Sed sit amet nisl sit amet lectus interdum iaculis. Proin at dolor id lacus rutrum suscipit.
+                Integer
+                vehicula urna at enim viverra fermentum. Pellentesque nec consectetur ipsum, eget accumsan est.
+                Donec turpis risus, posuere in euismod sed, maximus a felis. Nam aliquet sit amet tellus vitae
+                suscipit. Nullam varius massa id ex sagittis, eget pellentesque neque rhoncus. Nunc a mollis
+                erat,
+                sit amet tincidunt dui. Pellentesque tincidunt lorem ac justo aliquet faucibus. In hendrerit
+                ornare
+                mauris, in vulputate ipsum.
 
-                    Aliquam bibendum felis vitae lacus ullamcorper tristique. Aenean sed mattis ipsum. Integer
-                    consectetur non diam sit amet vehicula. Aenean interdum mollis neque ultricies tempus. Nunc a
-                    volutpat nisl, in tristique nunc. Proin non mi eu mi egestas commodo. Vestibulum quam diam,
-                    porttitor id dignissim vitae, viverra eu dolor. Suspendisse ornare, augue sed tincidunt accumsan, mi
-                    turpis accumsan lectus, in sodales libero velit ac purus. Suspendisse interdum semper magna at
-                    dapibus. Donec at nulla turpis.
+            </p>
+            <p>
 
-                    Nam sodales id massa ac malesuada. Cras turpis sem, sodales eu dignissim in, ultricies vel lorem.
-                    Etiam convallis ipsum nisl. Ut in semper enim. Nulla mollis neque vel est tempus, eu pellentesque
-                    risus pellentesque. Etiam sed volutpat est. Aenean eu ornare ligula.
-
-                    Suspendisse dolor ante, finibus at euismod quis, porttitor et nulla. In erat eros, tempus a
-                    facilisis sed, faucibus consequat est. Mauris venenatis enim ut ante molestie, feugiat convallis
-                    libero eleifend. Praesent sodales, ligula tempus fringilla iaculis, elit ex posuere lorem, ut
-                    lacinia turpis ex et orci. Proin varius mauris quam, sit amet rutrum ante dictum sed. Sed non metus
-                    sed dolor suscipit tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac
-                    gravida sem. Suspendisse potenti.
-
-                    Cras finibus eros sed sagittis aliquam. Donec hendrerit nec erat a dictum. Mauris sed erat in velit
-                    volutpat ultrices at ac lectus. Aliquam mollis, dui eu imperdiet luctus, ligula ipsum ultrices sem,
-                    vitae malesuada leo tortor sit amet enim. Nulla facilisi. Fusce pulvinar dui nec eros blandit, ac
-                    dictum ante dignissim. Aliquam quis pulvinar mi.
-
-                    Sed sit amet nisl sit amet lectus interdum iaculis. Proin at dolor id lacus rutrum suscipit. Integer
-                    vehicula urna at enim viverra fermentum. Pellentesque nec consectetur ipsum, eget accumsan est.
-                    Donec turpis risus, posuere in euismod sed, maximus a felis. Nam aliquet sit amet tellus vitae
-                    suscipit. Nullam varius massa id ex sagittis, eget pellentesque neque rhoncus. Nunc a mollis erat,
-                    sit amet tincidunt dui. Pellentesque tincidunt lorem ac justo aliquet faucibus. In hendrerit ornare
-                    mauris, in vulputate ipsum.
-
-                    Etiam rhoncus congue magna, nec congue eros viverra quis. Vivamus ut erat erat. Donec hendrerit nunc
-                    fermentum turpis sollicitudin rhoncus. Fusce aliquet interdum hendrerit. Vivamus laoreet ligula at
-                    pretium aliquam. Praesent hendrerit lorem rutrum sem tempor semper. Integer lacinia felis nisi, sit
-                    amet hendrerit nunc suscipit in. Pellentesque molestie ultrices orci, interdum vehicula augue mollis
-                    quis. Nullam at sodales mauris.
-                </p>
-            </header>
+                Etiam rhoncus congue magna, nec congue eros viverra quis. Vivamus ut erat erat. Donec hendrerit
+                nunc
+                fermentum turpis sollicitudin rhoncus. Fusce aliquet interdum hendrerit. Vivamus laoreet ligula
+                at
+                pretium aliquam. Praesent hendrerit lorem rutrum sem tempor semper. Integer lacinia felis nisi,
+                sit
+                amet hendrerit nunc suscipit in. Pellentesque molestie ultrices orci, interdum vehicula augue
+                mollis
+                quis. Nullam at sodales mauris.
+            </p>
         </div>
     );
 }

8ce64a194865f2639639aedb9a2dd8addf29b314
omri.g <omri.g@alibaba-inc.com>
2021-12-02 01:10:21 +0200

    - Started adding private mode which means the user can turn off the camera visibility and instead, present a winking emoji which updates live upon wink

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
+import React from 'react'
 import { useState, useEffect, useRef } from 'react'
 import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+import rightWinkEmoji from './assets/right_wink_emoji.png'
+import leftWinkEmoji from './assets/left_wink_emoji.png'
 import '@tensorflow/tfjs-backend-webgl';
 import './App.css';
 
-const EYE_ASPECT_RATIO_TH = 0.35
-const EYE_AR_CONSECUTIVE_FRAMES = 4
-const DEFAULT_SCROLL_BY_PIXELS = 55
+const EYE_ASPECT_RATIO_TH = 0.22
+const EYE_AR_CONSECUTIVE_FRAMES = 5
+const DEFAULT_SCROLL_BY_PIXELS = 120
 const VIDEO_HEIGHT = 250
 const VIDEO_WIDTH = 250
+const winkSide = Object.freeze({
+    right: 'right',
+    left: 'left',
+})
 
 const App = () => {
     const [model, setModel] = useState(null);
     // TODO: Add input component for changing this value
     const [scrollBy, setScrollBy] = useState(DEFAULT_SCROLL_BY_PIXELS);
+    const [privateMode, setPrivateMode] = useState(true);
     const videoRef = useRef(null)
     const streamRef = useRef(null)
     const winkCounterRef = useRef(0);
+    const [latestWinkSide, setLatestWinkSide] = useState(null);
     const currentYScrollPosition = useRef(0)
 
     useEffect(async () => {
         listenToWindowScroll();
-        await setupCamera();
         await loadModel();
+        await setupCamera();
         return (() => {
             streamRef.current.stop()
             window.removeEventListener('scroll', () => currentYScrollPosition.current = window.scrollY)
         })
     }, []);
-
-    // TODO: Export to utils and describe the function
-    const calculateEyeAspectRatio = (lowerEye, upperEye) => {
-        const a = calculateDistance(upperEye[4], lowerEye[3]);
-        const b = calculateDistance(upperEye[5], lowerEye[4]);
-        const c = calculateDistance(upperEye[0], upperEye[upperEye.length - 1]);
-
-        return (a + b) / (2.0 * c);
-    }
-
-    // TODO: Export to utils and describe the function
-    const calculateDistance = (point1, point2) => Math.hypot(point2[0] - point1[0], point2[1] - point1[1])
-
-    const listenToWindowScroll = () => {
-        window.addEventListener('scroll', () => currentYScrollPosition.current = window.scrollY)
-    }
 
     const loadModel = async () => {
         const faceModel = await faceLandmarksDetection.load(faceLandmarksDetection.SupportedPackages.mediapipeFacemesh)
@@ -61,6 +54,24 @@ const App = () => {
         }
     }
 
+    // TODO: Export to utils and describe the function
+    const calculateEyeAspectRatio = ({ lower, upper }) => {
+        // const a = calculateDistance(upper[2], lower[3]);
+        // const b = calculateDistance(upper[3], lower[4]);
+        const a = calculateDistance(upper[4], lower[5]);
+        const b = calculateDistance(upper[2], lower[3]);
+        const c = calculateDistance(lower[0], lower[lower.length - 1]);
+
+        return ((a + b) / (2.0 * c)).toFixed(2);
+    }
+
+    // TODO: Export to utils and describe the function
+    const calculateDistance = (point1, point2) => Math.hypot(point2[0] - point1[0], point2[1] - point1[1])
+
+    const listenToWindowScroll = () => {
+        window.addEventListener('scroll', () => currentYScrollPosition.current = window.scrollY)
+    }
+
     const isDesiredWinkingEye = (primaryEyeRatio, secondaryEyeRatio) =>
         primaryEyeRatio <= EYE_ASPECT_RATIO_TH && secondaryEyeRatio > EYE_ASPECT_RATIO_TH
 
@@ -71,8 +82,8 @@ const App = () => {
         });
     }
 
-    const isVoluntaryWink = () => {
-        if (winkCounterRef.current < EYE_AR_CONSECUTIVE_FRAMES) {
+    const isVoluntaryWink = winkSide => {
+        if (winkCounterRef.current < EYE_AR_CONSECUTIVE_FRAMES && winkSide === latestWinkSide) {
             winkCounterRef.current += 1
             return false
         }
@@ -81,36 +92,62 @@ const App = () => {
     }
 
     const handleWink = onWinkAction => {
-        isVoluntaryWink() && onWinkAction()
+        onWinkAction()
     }
 
     const processEyesLandmarks = (leftEyeUpper0, leftEyeLower0, rightEyeUpper0, rightEyeLower0) => {
-        const leftEyeAspectRatio = calculateEyeAspectRatio(leftEyeLower0, leftEyeUpper0)
-        const rightEyeAspectRatio = calculateEyeAspectRatio(rightEyeLower0, rightEyeUpper0)
-
+        const leftEyeAspectRatio = calculateEyeAspectRatio({ lower: leftEyeLower0, upper: leftEyeUpper0 })
+        const rightEyeAspectRatio = calculateEyeAspectRatio({ lower: rightEyeLower0, upper: rightEyeUpper0 })
+        // console.log(leftEyeAspectRatio, 'leftEyeAspectRatio');
+        // console.log(rightEyeAspectRatio, 'rightEyeAspectRatio');
         const isLeftEyeWink = isDesiredWinkingEye(leftEyeAspectRatio, rightEyeAspectRatio)
         const isRightEyeWink = isDesiredWinkingEye(rightEyeAspectRatio, leftEyeAspectRatio)
 
-        isLeftEyeWink && handleWink(() => scrollOnWinkAction(-scrollBy))
-        isRightEyeWink && handleWink(() => scrollOnWinkAction(scrollBy))
+        if (isLeftEyeWink && isVoluntaryWink(winkSide.left)) {
+            handleWink(() => scrollOnWinkAction(-scrollBy))
+            setLatestWinkSide(winkSide.left)
+        }
+        if (isRightEyeWink && isVoluntaryWink(winkSide.right)) {
+            handleWink(() => scrollOnWinkAction(scrollBy))
+            setLatestWinkSide(winkSide.right)
+        }
     }
 
     const detectWinks = async () => {
-        const predictions = model && videoRef.current && await model.estimateFaces({
-            input: videoRef.current,
-            predictIrises: false
-        });
-
-        predictions.forEach(({ annotations }) => {
-            const { leftEyeUpper0, leftEyeLower0, rightEyeUpper0, rightEyeLower0 } = annotations
-            processEyesLandmarks(leftEyeUpper0, leftEyeLower0, rightEyeUpper0, rightEyeLower0)
-        })
-
+        const predictions = model && videoRef.current && await model.estimateFaces({ input: videoRef.current });
+        if (predictions && predictions.length) {
+            predictions.forEach(({ annotations }) => {
+                const { leftEyeUpper0, leftEyeLower0, rightEyeUpper0, rightEyeLower0 } = annotations
+                processEyesLandmarks(leftEyeUpper0, leftEyeLower0, rightEyeUpper0, rightEyeLower0)
+            })
+        }
         requestAnimationFrame(detectWinks)
     }
 
     return (
         <div className="App">
+            <div className={'stream-video'}>
+                <div>
+                    <button onClick={() => setPrivateMode(!privateMode)}>Private Mode</button>
+                    {privateMode
+                    && <img
+                        src={latestWinkSide === winkSide.right ? rightWinkEmoji : leftWinkEmoji}
+                        alt={'wink_emoji'}
+                        className={'wink-emoji'}
+                    />}
+                </div>
+                <video
+                    style={{ 'visibility': privateMode ? 'hidden' : 'visible'}}
+                    ref={videoRef}
+                    id={'stream-video'}
+                    playsInline
+                    autoPlay
+                    muted
+                    width={VIDEO_WIDTH}
+                    height={VIDEO_HEIGHT}
+                    onLoadedData={detectWinks}
+                />
+            </div>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras auctor sodales tempus. Sed ante
                 libero, scelerisque a pulvinar non, mattis eu nibh. Donec bibendum euismod lorem quis laoreet.
@@ -209,17 +246,6 @@ const App = () => {
                 mollis
                 quis. Nullam at sodales mauris.
             </p>
-            <video
-                ref={videoRef}
-                id={'stream-video'}
-                playsInline
-                autoPlay
-                muted
-                width={VIDEO_WIDTH}
-                height={VIDEO_HEIGHT}
-                className={'stream-video'}
-                onLoadedData={detectWinks}
-            />
         </div>
     );
 }
